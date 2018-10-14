b99e7de85062938710587c57f6cb760745201f00
alaniz <alaniz@linuxmail.org>
2018-10-14 14:58:35 +0100

    Added pywebview support

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 MemeChain GUI https://kekcoin.co/memechain
 
 This is the development repository for the Memechain GUI. THIS SOFTWARE IS IN DEVELOPMENT AND SHOULD NOT BE USED IN PRODUCTION.
+
+## Installation
+
+This installation guide will be using Linux/Unix throughout.
+
+### Python Dependencies
+
+Python will need to be installed on your system. To install python-pip run
+
+```
+sudo apt install python-pip
+```
+
+Once you have python-pip installed you will need to install the python dependencies by running
+
+```
+pip install flask pypiwin32 pythonnet comtypes pywebview
+```
--- app.py
+++ app.py
@@ -4,19 +4,25 @@ import flask
 
 # Debug
 # Set this to 'False' in a production environment.
-debug = True
+debug = False
 
 # Initialize Flask App
 app = flask.Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1 # disable caching
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store'
+    return response
 
 # Index route: renders the landing/home page from templates.
 @app.route("/")
 def index():
     return flask.render_template("index.html", isform=False)
 
+def run_server():
+    app.run(host='127.0.0.1', port=1133, debug=debug, use_reloader=False, threaded=True)
+
 # Run app
 if __name__ == '__main__':
-    if debug:
-        app.run(host='127.0.0.1', port=1133, debug=True)
-    else:
-        app.run()
+    run_server()
